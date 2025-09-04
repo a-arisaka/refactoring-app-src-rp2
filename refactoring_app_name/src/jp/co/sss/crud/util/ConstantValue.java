444b0e0bbdc4e628165d104d1b2c2ff2ebf7621d
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-04 16:46:31 +0900

    fix:utilの変更
    クラスをSQLに細分化します

--- refactoring_app_name/src/jp/co/sss/crud/util/ConstantValue.java
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantValue.java
@@ -1,5 +1,15 @@
 package jp.co.sss.crud.util;
 
 public class ConstantValue {
+	public static final String JDBC_DRIVER = "org.postgresql.Driver";
+
+	/** DB接続用文字列 */
+	public static final String DB_URL = "jdbc:postgresql://localhost:5432/rp2db";
+
+	/** DB接続用ユーザー */
+	public static final String USER_NAME = "console_crud_user";
+
+	/** DB接続用パスワード */
+	public static final String PASSWORD = "systemsss";
 
 }
