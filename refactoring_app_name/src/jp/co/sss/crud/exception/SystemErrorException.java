1f3aa270fe3b75ee8304d6157c4ad872e85dde36
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-05 16:30:09 +0900

    fix

--- refactoring_app_name/src/jp/co/sss/crud/exception/SystemErrorException.java
+++ refactoring_app_name/src/jp/co/sss/crud/exception/SystemErrorException.java
@@ -1,13 +1,13 @@
 package jp.co.sss.crud.exception;
 
-public class SystemErrorException extends Exception{
-//例外が起きたときの処理を記入
-	  /**
-     * コンストラクタ
-     * @param message エラーメッセージ
-     * @param cause 元となった例外 (スタックトレースを維持するため)
-     */
-    public SystemErrorException(String message, Throwable cause) {
-        super(message, cause);
-    }
+public class SystemErrorException extends Exception {
+	//例外が起きたときの処理を記入
+	/**
+	* コンストラクタ
+	* @param message エラーメッセージ
+	* @param cause 元となった例外
+	*/
+	public SystemErrorException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
