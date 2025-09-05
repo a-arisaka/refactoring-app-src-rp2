5b98185d46bd5c2b9a7fe1b4d012bac746d6cc86
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-05 15:15:41 +0900

    fix MenuNoReaderの編集
    エラーメッセージの実装

--- refactoring_app_name/src/jp/co/sss/crud/io/MenuNoReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/MenuNoReader.java
@@ -1,17 +1,21 @@
 package jp.co.sss.crud.io;
 
-public class MenuNoReader implements IConsoleReader{
+public class MenuNoReader implements IConsoleReader {
 
 	@Override
 	public String getErrorMsg() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		return "メニュー番号を入力してください";
 	}
 
 	@Override
 	public boolean isValid(String inputString) {
-		// TODO 自動生成されたメソッド・スタブ
-		return false;
+		// nullまたは空文字列の場合は無効
+		if (inputString == null || inputString.isEmpty()) {
+			return false;
+		}
+		// 正規表現を使い、文字列が1文字以上の半角数字で構成されているかチェック
+		return inputString.matches("\\d+");
+
 	}
 
 	@Override
