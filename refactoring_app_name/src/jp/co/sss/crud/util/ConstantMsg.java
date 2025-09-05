7a632b84ec8910b6cd4ecd92cd3c347abaabda92
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-05 17:29:33 +0900

    fix ConstantMsgの編集
    コンソールに出力するメッセージの定数を追加

--- refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -14,4 +14,21 @@ public class ConstantMsg {
 	public static final int MENU_DELETE = 6;
 	
 	public static final String MSG_SYSTEM_ERROR = "エラーが発生しました";
+	public static final String MSG_UNKNOWN = "該当者はいませんでした";
+	public static final String MSG_RECORD = "社員ID\\t社員名\\t性別\\t生年月日\\t部署名";
+	public static final String GENDER_UNKNOWN = "\"回答なし\" + \"\\t\"";
+	public static final String GENDER_MALE = "男性" + "\t";
+	public static final String GENDER_FEMALE = "女性" + "\t";
+	public static final String GENDER_OTHER = "その他" + "\t";
+	public static final String DEPT_ID_1 = "営業部";
+	public static final String DEPT_ID_2 = "経理部";
+	public static final String DEPT_ID_3 = "総務部";
+	public static final String REGISTER_COMPLETE = "社員情報を登録しました";
+	public static final String UPDATE_COMPLETE = "社員情報を更新しました";
+	public static final String DELETE_COMPLETE = "社員情報を削除しました";
+	
+	
+	
+			
+	
 }
