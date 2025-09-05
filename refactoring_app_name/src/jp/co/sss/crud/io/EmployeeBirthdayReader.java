2ac001e1f09027f5589064578cea316e037742d0
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-05 15:32:34 +0900

    fix EmployeeBirthdayReaderの編集
    メソッドのオーバーライド
    正規表現の追加

--- refactoring_app_name/src/jp/co/sss/crud/io/EmployeeBirthdayReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/EmployeeBirthdayReader.java
@@ -1,5 +1,25 @@
 package jp.co.sss.crud.io;
 
-public class EmployeeBirthdayReader {
+public class EmployeeBirthdayReader implements IConsoleReader {
 
+	@Override
+	public String getErrorMsg() {
+		return "生年月日を入力してください";
+	}
+
+	@Override
+	public boolean isValid(String inputString) {
+		// nullまたは空文字列の場合は無効
+		if (inputString == null || inputString.isEmpty()) {
+			return false;
+		}
+		// 正規表現
+		return inputString.matches("^[0-9]{4}/(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])$");
+
+	}
+
+	@Override
+	public boolean isParseInt() {
+		return false;
+	}
 }
