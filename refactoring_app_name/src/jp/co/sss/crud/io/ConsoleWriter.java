a787c91895302b6cd3ab1927d957d53ffae68040
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-05 17:51:52 +0900

    fix ConsoleWriterの編集
    メッセージ、DTOを表示するprint()メソッドを追加

--- refactoring_app_name/src/jp/co/sss/crud/io/ConsoleWriter.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/ConsoleWriter.java
@@ -1,5 +1,31 @@
 package jp.co.sss.crud.io;
 
 public class ConsoleWriter {
-
+	/*	・printメソッドをラッピングし、コンソールにメッセージを出力する。メッセージはConstantsクラスにまとめる 
+		・DTOのコンソール出力はtoStringメソッドを活用する。 
+	*/
+	 /**
+     * メッセージとDTOオブジェクトを組み合わせてコンソールに出力
+     * @param ConstantMsgから渡された定数を表示
+     * @param dto 表示するDTOオブジェクト
+     */
+ public void print(String message, Object dto) {
+	 System.out.println(message + dto);
+ }
+ /**
+  * DTOオブジェクト単体をコンソールに出力
+  * @param dto 表示するDTOオブジェクト（toString()メソッドが呼ばれる）
+  */
+ public void print(Object dto) {
+ 
+     System.out.println(dto);
+ }
+ 
+ /**
+  * メッセージ単体でコンソールに出力
+  * @param ConstantMsgから渡された定数
+  */
+ public void print(String message) {
+	 System.out.println(message);
+ }
 }
