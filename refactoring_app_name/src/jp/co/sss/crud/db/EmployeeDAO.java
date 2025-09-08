ea2681c36a55888f0938fa89dedf767255b27fdf
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-08 15:46:16 +0900

    fix DAOの編集
    find insert update delete それぞれのメソッドを更新

--- refactoring_app_name/src/jp/co/sss/crud/db/EmployeeDAO.java
+++ refactoring_app_name/src/jp/co/sss/crud/db/EmployeeDAO.java
@@ -13,66 +13,214 @@ import jp.co.sss.crud.dto.Employee;
 import jp.co.sss.crud.exception.SystemErrorException;
 import jp.co.sss.crud.util.ConstantSQL;
 
-public class EmployeeDAO implements IEmployeeDAO{
+public class EmployeeDAO implements IEmployeeDAO {
+	/** インスタンス化を禁止 */
+	private EmployeeDAO() {
 
-	@Override
-	public List<Employee> findAll() throws SystemErrorException {		
-		   List<Employee> employeeList = new ArrayList<>();
-	        Connection connection = null;
-	        PreparedStatement preparedStatement = null;
-	        ResultSet resultSet = null;
-
-	        try {
-	            
-	            connection = DBManager.getConnection();
-	            preparedStatement = connection.prepareStatement(ConstantSQL.SQL_ALL_SELECT);
-	            resultSet = preparedStatement.executeQuery();
-	            
-	            while (resultSet.next()) {
-	            	  int empId = resultSet.getInt("emp_id");
-	                  String empName = resultSet.getString("emp_name");
-	                  int gender = resultSet.getInt("gender");
-	                  Date birthday = resultSet.getDate("birthday");
-	                  Department deptName = (Department) resultSet.getObject("depe_name");
-	                  
-	                  Employee employee = new Employee(empId, empName, gender, birthday, deptName);
-	                  employeeList.add(employee);
-	            }
-
-	        } catch (SQLException | ClassNotFoundException e) { 
-	            // チェック例外であるSystemErrorExceptionをスロー
-	            throw new SystemErrorException("システムエラー: データベース処理中に問題が発生しました。", e);
-	        } finally {
-	         
-	        }
-	        return employeeList;
 	}
 
 	@Override
+	public List<Employee> findAll() throws SystemErrorException {
+		List<Employee> employeeList = new ArrayList<>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			//DBに接続
+			connection = DBManager.getConnection();
+			//ステートメントを作成
+			preparedStatement = connection.prepareStatement(ConstantSQL.SQL_ALL_SELECT);
+			//SQL文を実行
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				int empId = resultSet.getInt("emp_id");
+				String empName = resultSet.getString("emp_name");
+				int gender = resultSet.getInt("gender");
+				Date birthday = resultSet.getDate("birthday");
+				Department deptName = (Department) resultSet.getObject("depe_name");
+
+				Employee employee = new Employee(empId, empName, gender, birthday, deptName);
+				employeeList.add(employee);
+			}
+
+		} catch (SQLException | ClassNotFoundException e) {
+			// チェック例外であるSystemErrorExceptionをスロー
+			throw new SystemErrorException("システムエラー: データベース処理中に問題が発生しました。", e);
+		}
+		return employeeList;
+	}
+
+	/**
+	 * 社員名検索
+	 * 
+	 * @param searchName 検索社員名 
+	 * @return {@code List<Employee>} 検索社員名を含むエンティティリスト
+	 * @throws SystemErrorException : {@code ClassNotFoundException | SQLException }をキャッチしてスローする
+	 */
+	@Override
 	public List<Employee> findByEmployeeName(String searchName) throws SystemErrorException {
-		return null;
+		List<Employee> employeeList = new ArrayList<>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+
+		try {
+			//DBに接続
+			connection = DBManager.getConnection();
+			// SQL文を準備
+			StringBuffer sql = new StringBuffer(ConstantSQL.SQL_SELECT_BASIC);
+			sql.append(ConstantSQL.SQL_SELECT_BY_EMP_NAME);
+
+			// ステートメントの作成
+			preparedStatement = connection.prepareStatement(sql.toString());
+
+			// 検索条件となる値をバインド
+			preparedStatement.setString(1, "%" + ((Employee) employeeList).getEmpName() + "%");
+
+			// SQL文を実行
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				int empId = resultSet.getInt("emp_id");
+				String empName = resultSet.getString("emp_name");
+				int gender = resultSet.getInt("gender");
+				Date birthday = resultSet.getDate("birthday");
+				Department deptName = (Department) resultSet.getObject("depe_name");
+
+				Employee employee = new Employee(empId, empName, gender, birthday, deptName);
+				employeeList.add(employee);
+			}
+
+		} catch (SQLException | ClassNotFoundException e) {
+			// チェック例外であるSystemErrorExceptionをスロー
+			throw new SystemErrorException("システムエラー: データベース処理中に問題が発生しました。", e);
+		}
+		return employeeList;
 	}
 
 	@Override
 	public List<Employee> findByDeptId(int deptId) throws SystemErrorException {
-		return null;
+		List<Employee> employeeList = new ArrayList<>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		try {
+			// DBに接続
+			connection = DBManager.getConnection();
+
+			// SQL文を準備
+			StringBuffer sql = new StringBuffer(ConstantSQL.SQL_SELECT_BASIC);
+			sql.append(ConstantSQL.SQL_SELECT_BY_DEPT_ID);
+
+			// ステートメントの作成
+			preparedStatement = connection.prepareStatement(sql.toString());
+
+			// 検索条件となる値をバインド
+			preparedStatement.setInt(1, deptId);
+
+			// SQL文を実行
+			resultSet = preparedStatement.executeQuery();
+
+			while (resultSet.next()) {
+				int empId = resultSet.getInt("emp_id");
+				String empName = resultSet.getString("emp_name");
+				int gender = resultSet.getInt("gender");
+				Date birthday = resultSet.getDate("birthday");
+				Department deptName = (Department) resultSet.getObject("depe_name");
+
+				Employee employee = new Employee(empId, empName, gender, birthday, deptName);
+				employeeList.add(employee);
+			}
+
+		} catch (SQLException | ClassNotFoundException e) {
+			// チェック例外であるSystemErrorExceptionをスロー
+			throw new SystemErrorException("システムエラー: データベース処理中に問題が発生しました。", e);
+		}
+		return employeeList;
 	}
 
 	@Override
 	public void insert(Employee employee) throws SystemErrorException {
-		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			// DBに接続
+			connection = DBManager.getConnection();
+
+			// ステートメントを作成
+			preparedStatement = connection.prepareStatement(ConstantSQL.SQL_INSERT);
+
+			preparedStatement.setString(1, employee.getEmpName());
+			preparedStatement.setInt(2, employee.getGender());
+			java.util.Date utilBirthday = employee.getBirthday();
+			java.sql.Date sqlBirthday = new java.sql.Date(utilBirthday.getTime());
+			preparedStatement.setDate(3, sqlBirthday);
+
+			// Departmentオブジェクトから部署IDを取得してセット
+			preparedStatement.setInt(4, employee.getDepartment().getDeptId());
+
+			// INSERT文を実行
+			preparedStatement.executeUpdate();
+		} catch (SQLException | ClassNotFoundException e) {
+			// チェック例外であるSystemErrorExceptionをスロー
+			throw new SystemErrorException("システムエラー: データベース処理中に問題が発生しました。", e);
+		}
+
 	}
 
 	@Override
 	public Integer update(Employee employee) throws SystemErrorException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
 		
-		return null;
+		try {
+			// データベースに接続
+			connection = DBManager.getConnection();
+
+			// ステートメントの作成
+			preparedStatement = connection.prepareStatement(ConstantSQL.SQL_UPDATE);
+
+			preparedStatement.setString(1, employee.getEmpName());
+			preparedStatement.setInt(2, employee.getGender());
+			java.util.Date utilBirthday = employee.getBirthday();
+			java.sql.Date sqlBirthday = new java.sql.Date(utilBirthday.getTime());
+			preparedStatement.setDate(3, sqlBirthday);
+
+			// Departmentオブジェクトから部署IDを取得してセット
+			preparedStatement.setInt(4, employee.getDepartment().getDeptId());
+
+			// SQL文の実行(失敗時は戻り値0)
+			preparedStatement.executeUpdate();
+		} catch (SQLException | ClassNotFoundException e) {
+			// チェック例外であるSystemErrorExceptionをスロー
+			throw new SystemErrorException("システムエラー: データベース処理中に問題が発生しました。", e);
+		}
+		return 1;
 	}
 
 	@Override
 	public Integer delete(Integer empId) throws SystemErrorException {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		try {
+			// データベースに接続
+			connection = DBManager.getConnection();
+
+			// ステートメントの作成
+			preparedStatement = connection.prepareStatement(ConstantSQL.SQL_DELETE);
+
+			// 社員IDをバインド
+			preparedStatement.setInt(1, empId);
+			
+			preparedStatement.executeUpdate();
+		} catch (SQLException | ClassNotFoundException e) {
+			// チェック例外であるSystemErrorExceptionをスロー
+			throw new SystemErrorException("システムエラー: データベース処理中に問題が発生しました。", e);
+		}
 		
-		return null;
+		return 1;
 	}
 
 }
