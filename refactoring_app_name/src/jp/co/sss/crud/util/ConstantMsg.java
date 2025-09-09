263e558f8aaf5bf70d11aac14b8a72335c05eb47
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-09 14:13:09 +0900

    fix 定数の更新
    ConstantMsgクラスに新たな定数を追加
    ConstantSQLクラスに社員IDで検索する定数を追加

--- refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -12,12 +12,22 @@ public class ConstantMsg {
 	public static final int MENU_INSERT = 4;
 	public static final int MENU_UPDATE = 5;
 	public static final int MENU_DELETE = 6;
-	
+
+	public static final String EMP_NAME = "社員名";
+	public static final String EMP_ID = "社員ID";
+	public static final String GENDER = "性別(0:その他, 1:男性, 2:女性, 9:回答なし):";
+	public static final String BIRTHDAT = "生年月日(西暦年/月/日):";
+	public static final String DEPT_ID = "部署ID(1:営業部、2:経理部、3:総務部):";
+
+	public static final String MSG_UPDATE_EMP_ID = "更新する社員の社員IDを入力してください：";
+	public static final String MSG_DEPT_ID = "部署ID(1:営業部、2:経理部、3:総務部)を入力してください:";
+	public static final String MSG_DELETE_EMP_ID = "削除する社員の社員IDを入力してください：";
+
 	public static final String MSG_SYSTEM_ERROR = "エラーが発生しました";
 	public static final String MSG_UNKNOWN = "該当者はいませんでした";
 	public static final String MSG_RECORD = "社員ID\\t社員名\\t性別\\t生年月日\\t部署名";
 	public static final String GENDER_UNKNOWN = "\"回答なし\" + \"\\t\"";
-	public static final String GENDER_MALE= "男性" + "\t";
+	public static final String GENDER_MALE = "男性" + "\t";
 	public static final String GENDER_FEMALE = "女性" + "\t";
 	public static final String GENDER_OTHER = "その他" + "\t";
 	public static final String DEPT_ID_1 = "営業部";
@@ -26,9 +36,5 @@ public class ConstantMsg {
 	public static final String REGISTER_COMPLETE = "社員情報を登録しました";
 	public static final String UPDATE_COMPLETE = "社員情報を更新しました";
 	public static final String DELETE_COMPLETE = "社員情報を削除しました";
-	
-	
-	
-			
-	
+
 }
