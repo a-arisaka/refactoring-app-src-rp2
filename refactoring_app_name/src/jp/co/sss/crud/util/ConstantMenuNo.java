eb9293d2588b725ee722891252835fcd15ac3064
a-arisaka <aya-arisaka@kiseki.systems>
2025-09-10 14:14:00 +0900

    add ConstantMenuNoを追加
    メニュー番号を定数化

---
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantMenuNo.java
@@ -0,0 +1,14 @@
+package jp.co.sss.crud.util;
+
+public class ConstantMenuNo {
+	private ConstantMenuNo() {
+	}
+	//Mainで使うメニュー番号を定数化
+	public static final int MENU_SELECT_ALL = 1;
+	public static final int MENU_SEARCH_EMP_NAME = 2;
+	public static final int MENU_SEARCH_DEPT_ID = 3;
+	public static final int MENU_INSERT = 4;
+	public static final int MENU_UPDATE = 5;
+	public static final int MENU_DELETE = 6;
+	public static final int MENU_EXIT = 7;
+}
